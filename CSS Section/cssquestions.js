4c7706c74fe6ea08a3e63e716de74bb48bf1bf69
YashMishra0101 <yashrkm8822@gmail.com>
2024-11-17 23:21:50 +0530

    working on css

--- CSS Section/cssquestions.js
+++ CSS Section/cssquestions.js
@@ -57,13 +57,54 @@ like variables, selector nesting, and more complex operations, making it a popul
 organized, maintainable CSS. Preprocessor code compiles into standard CSS, ready for the browser.
 
 # 3)What is border size border box ?
-Yes, exactly! In simple words:
-
 When you use `box-sizing: border-box` in CSS, adding padding or borders **won’t change the actual height and 
 width** of an element. The total size remains as you set it (e.g., 500px height and 500px width).  
 
-Without `box-sizing: border-box` (the default is `content-box`), padding and borders **will increase** 
+Without `box-sizing: border-box` padding and borders **will increase** 
 the total height and width, making the element larger than what you initially set.  
 
 By default, `box-sizing` is set to `content-box`.
+
+--box-sizing:border-box;
+--box-sizing:content-box;(By default)
+
+# 4)Position Property In CSS ?
+
+  position: static (Default);
+  position: relative;
+  position: absolute;
+  position: fixed;
+  position: sticky;
+  
+There are Four Main Postion Property :
+
+1. `static` (Default):  
+   - This is the default position for elements.  
+
+2. `relative`:  
+   - You can move it using properties like top, right, bottom, or left.
+
+3. `absolute`:  
+   - The element moves based on its nearest parent with a position set (like `relative`)**.  
+   - If there’s no such parent, it moves based on the whole page (`body`). 
+   - absolute relies on a relative parent; otherwise, it uses the body.
+ 
+
+4. `fixed`:  
+   - The element stays in a fixed spot on the screen, no matter how much you scroll.  
+   - If top, bottom, left, or right are used, the element is positioned relative to the viewport.(browser window). 
+
+5. `sticky`:  
+   - The element sticks in place while you scroll within itsn nearest parent container.  
+   - It behaves like `relative` at first, then sticks like `fixed` until the parent’s boundary is reached.  
+   - It "sticks" to a specified offset (e.g., top: 0) within its parent container and stops sticking once it reaches the parent's boundary.
+
+--Key Differences Between Fixed and Sticky :
+
+| Property         | `fixed`                            | `sticky`                           |
+|------------------|------------------------------------|------------------------------------|
+| Reference        | Viewport                          | Parent container boundaries         |
+| Scroll Behavior  | Always stays fixed                | Becomes fixed within a boundary     |
+| Common Use       | Navigation bars, floating buttons | Table headers, sticky sections      |
+
 */
