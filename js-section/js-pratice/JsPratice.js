f465f1ad4e3b14ab643ff5232696d1f36ffe09ef
YashMishra0101 <yashrkm8822@gmail.com>
2025-03-13 23:36:15 +0530

    working on js

--- js-section/js-pratice/JsPratice.js
+++ js-section/js-pratice/JsPratice.js
@@ -1,77 +1,26 @@
-// let isEven = (value) => {
-//   let check = value % 2 === 0;
-//   check
-//     ? console.log(`${value} is a Even Number`)
-//     : console.log(`${value} is Not a Even Number`);
-// };
-
-// isEven(false);
-
-// let printEven = (value) => {
-//   let a = 0;
-//   for (let check = a; check <= value; check++) {
-//     for (let print = check; print % 2 === 0; print++) {
-//       console.log(print);
+// let checkPrime = (num) => {
+//   for (let a = 2; a < num ; a++) {
+//     for (let b = a; num % b === 0 ; b++) {
+//       console.log("Not a Prime Number");
+//       return;
 //     }
 //   }
+//   console.log("It's a Prime Number");
 // };
 
-// printEven(7);
-
-// let isOdd = (value) => {
-//   let check = value % 2 !== 0;
-//   check
-//     ? console.log(`${value} is a odd Number`)
-//     : console.log(`${value} is Not a odd Number`);
-// };
-
-// isOdd(9);
-
-// let printOdd = (value) => {
-//   let a = 0;
-//   for (check = a; check<=value; check++) {
-//     for (let print = check; print % 2 !== 0; print++) {
-//       console.log(print);
-//     }
-//   }
-// };
-
-// printOdd(9)
-
-// function checkEvenOdd(value) {
-//   if (typeof value !== 'number' || isNaN(value)) {
-//     console.log(`${value} is not a valid number.`);
-//     return;
-//   }
-//   if (!Number.isInteger(value)) {
-//     console.log(`${value} is not an integer; please provide an integer value.`);
-//     return;
-//   }
-//   if (value % 2 === 0) {
-//     console.log(`${value} is an Even Number`);
-//   } else {
-//     console.log(`${value} is an Odd Number`);
-//   }
-// }
-
-// // Example usage:
-// checkEvenOdd(7);   // Output: 7 is an Odd Number
-// checkEvenOdd(8);   // Output: 8 is an Even Number
-// checkEvenOdd(7.5); // Output: 7.5 is not an integer; please provide an integer value.
-// checkEvenOdd('a'); // Output: a is not a valid number.
-
-let checkNumber = (value) => {
-  if (typeof value !== "number" || !Number.isInteger(value)){
-    console.log(`${value} is not a valid number`);
-    return;
-  }
-
-  if (value % 2 === 0) {
-    console.log(`${value} is a Even NUmber`);
-  } else {
-    console.log(`${value} is a Odd Number`);
-  }
-};
+// checkPrime(13);
 
 
-checkNumber(NaN)
+let checkPrime = (num) => {
+    
+      for (let b = 2; num % b === !0 ; b++) {
+          for (let a = b; a < num ; a++) {
+              console.log("It's a Prime Number");
+              return;
+            }
+        }
+        console.log("Not a Prime Number");
+      
+  };
+  
+  checkPrime(7);
--- js-section/js-questions/JsAnswer.js
+++ js-section/js-questions/JsAnswer.js
@@ -1,3 +1,79 @@
 console.log("Js Question Section");
 
+/*
+#Ans 1)
 
+--Checking Even or Odd Number
+
+let checkNumber = (num) => {
+  if (typeof num !== "number" || !Number.isInteger(num)) {
+    console.log("This is not a valid Number");
+    return;
+  }
+
+  if (num % 2 === 0) {
+    console.log("It's a even number");
+  } else {
+    console.log("It's a odd number");
+  }
+};
+
+checkNumber(8);
+
+---Printing Even Number Betwee the two number
+
+let printEven = (numOne, numTwo) => {
+  if (
+    typeof !numOne === "number" ||
+    typeof !numTwo === "number" ||
+    !Number.isInteger(numOne) ||
+    !Number.isInteger(numTwo)
+  ) {
+    console.log("Please Enter a Valid Number");
+    return;
+  }
+
+  if (numOne >= numTwo) {
+    console.log("Number-One Must be Less than Number Two");
+    return;
+  }
+
+  if (numOne % 2 !== 0) {
+    numOne++;
+  }
+  for (let a = numOne; numOne <= numTwo; numOne += 2) {
+    console.log(numOne);
+  }
+};
+
+printEven(3, 8);
+
+---Printing Odd Number Betwee the two number
+
+let printOdd = (numOne, numTwo) => {
+  if (
+    typeof !numOne === "number" ||
+    typeof !numTwo === "number" ||
+    !Number.isInteger(numOne) ||
+    !Number.isInteger(numTwo)
+  ) {
+    console.log("Please Enter a Valid Number");
+    return;
+  }
+
+  if (numOne >= numTwo) {
+    console.log("Number-One Must be Less than Number Two");
+    return;
+  }
+
+  if (numOne % 2 === 0) {
+    numOne++;
+  }
+  for (let a = numOne; numOne <= numTwo; numOne += 2) {
+    console.log(numOne);
+  }
+};
+
+printOdd(2, 8);
+
+*/
