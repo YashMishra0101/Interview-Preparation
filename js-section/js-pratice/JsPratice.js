579102bb44240fc96cbb3d23df8b0a10e20d0e81
YashMishra0101 <yashrkm8822@gmail.com>
2025-03-27 23:56:44 +0530

    working on pattern questions

--- js-section/js-pratice/JsPratice.js
+++ js-section/js-pratice/JsPratice.js
@@ -1,4 +1,12 @@
-console.log("JS Practice Room");
+let print = (value) => {
+  let store = "";
+  for (let a = 1; a <= value; a++) {
+    for (let b = 1; b <= a; b++) {
+      store += " * ";
+    }
+    store += "\n";
+  }
+  return store;
+};
 
-
-
+console.log(print(5));
--- js-section/js-questions/JsAnswer.js
+++ js-section/js-questions/JsAnswer.js
@@ -137,4 +137,20 @@ let printPattern=(num)=>{
     }
     
     console.log(printPattern(5));
+
+----2)
+
+let print = (value) => {
+  let store = "";
+  for (let a = 1; a <= value; a++) {
+    for (let b = 1; b <= a; b++) {
+      store += " * ";
+    }
+    store += "\n";
+  }
+  return store;
+};
+
+console.log(print(5));
+
 */
