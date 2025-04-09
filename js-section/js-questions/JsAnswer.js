5f4de30a95948b33263450653b4ec34954d88a46
YashMishra0101 <yashrkm8822@gmail.com>
2025-04-09 22:28:14 +0530

    working on pattern questions

--- js-section/js-questions/JsAnswer.js
+++ js-section/js-questions/JsAnswer.js
@@ -404,4 +404,28 @@ let patternSixteen=(n)=>{
 
 console.log(patternSixteen(5))
 
+---17)
+
+let patternSevenTeen = (n) => {
+    let store = "";
+    for (let a = 1; a <= n; a++) {
+      let alphabet = 65;
+      for (let space = 1; space <= n - a; space++) {
+        store += " ";
+      }
+      for (let b = 1; b <= 2 * a - 1; b++) {
+        if (b > a) {
+          store += String.fromCharCode(alphabet-2);
+        } else {
+          store += String.fromCharCode(alphabet);
+          alphabet++;
+        }
+      }
+      store += "\n";
+    }
+    return store;
+  };
+  
+  console.log(patternSevenTeen(5));
+
 */
