dcdc49defe6e8211d7099e40a33aeec820d0f3b1
YashMishra0101 <yashrkm8822@gmail.com>
2025-04-29 02:02:25 +0530

    minor imporvement in last solution

--- js-section/js-pratice/JsPratice.js
+++ js-section/js-pratice/JsPratice.js
@@ -2,16 +2,16 @@ console.log("Js Practic is working");
 
 let partternTwentyThree = (n) => {
   let size = 2 * n - 1;
-  let store ="";
+  let store = "";
   for (let i = 0; i < size; i++) {
     for (let j = 0; j < size; j++) {
       let minimumNumber = Math.min(i, j, size - i - 1, size - j - 1);
-      print = n - minimumNumber;
+      let print = n - minimumNumber;
       store += print + " ";
     }
-    store+="\n";
+    store += "\n";
   }
-  return store.trim();
+  return store;
 };
 
 console.log(partternTwentyThree(4));
--- js-section/js-questions/JsAnswer.js
+++ js-section/js-questions/JsAnswer.js
@@ -593,16 +593,16 @@ console.log(patternTwentyOne(5))
 
 let partternTwentyThree = (n) => {
   let size = 2 * n - 1;
-  let store ="";
+  let store = "";
   for (let i = 0; i < size; i++) {
     for (let j = 0; j < size; j++) {
       let minimumNumber = Math.min(i, j, size - i - 1, size - j - 1);
-      print = n - minimumNumber;
+      let print = n - minimumNumber;
       store += print + " ";
     }
-    store+="\n";
+    store += "\n";
   }
-  return store.trim();
+  return store;
 };
 
 console.log(partternTwentyThree(4));
