f5f38206c0977b6d5fe4d5a3c3110e906d43652d
YashMishra0101 <yashrkm0011@gmail.com>
2025-05-29 23:55:25 +0530

    birthday

--- js-section/js-pratice/JsPratice.js
+++ js-section/js-pratice/JsPratice.js
@@ -1,18 +1,3 @@
 console.log("Js Practic is working");
 
 
-let str = "zZabcdedcjbA";
-
-let count = {};
-
-for (let a = 0; a < str.length; a++) {
-  let char = str[a].toLocaleLowerCase();
-
-  if (count[char]) {
-    count[char]++;
-  } else {
-    count[char] = 1;
-  }
-}
-
-console.log(count);
