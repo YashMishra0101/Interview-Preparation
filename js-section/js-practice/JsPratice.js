e913e53494332b67374011b1e0b88d853ea8123c
YashMishra0101 <yashrkm0011@gmail.com>
2025-07-08 22:39:44 +0530

    Interview prep

---
+++ js-section/js-practice/JsPratice.js
@@ -0,0 +1,29 @@
+console.log("Working");
+
+
+
+
+// let userInfo={
+//     naam:"Yash",
+//     age:"23",
+
+//     info:()=>{
+//         console.log(`${this.naam} ${this.age}`);
+//     }
+// }
+
+// userInfo.info()
+
+// console.log(this);
+
+// function abc(){
+//     console.log(this);
+// }
+
+const a=()=>{
+    let naam="Yash"
+    console.log(this);
+}
+a()
+
+// abc()
