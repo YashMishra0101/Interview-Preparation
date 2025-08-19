6d1c4945f9ea460592d0f48a80785f5b14cff950
YashMishra0101 <yashrkm0011@gmail.com>
2025-08-19 21:26:07 +0530

    working

--- js-section/js-practice/JsPractice.js
+++ js-section/js-practice/JsPractice.js
@@ -1,14 +1,2 @@
 console.log("Working");
 
-// let randomNumber=(num1,num2)=>{
-// return Math.floor(Math.random()*(num2-num1+1)+num1)
-// }
-
-// let answer=randomNumber(2,8);
-
-// console.log(answer);
-
-//(0*(7)+2)
-
-console.log([] == false);  
-console.log([] === false);
