38c5979e49182aa49f5e2a0a7390be7d6fa236a0
YashMishra0101 <yashrkm0011@gmail.com>
2025-09-05 01:11:34 +0530

    Working on Problem Solving

--- js-section/js-practice/JsPractice.js
+++ js-section/js-practice/JsPractice.js
@@ -1,15 +1,5 @@
 console.log("Working");
 // 'use strict'
 
-// const crypto = require("crypto");
-
-// function generateSixDigitOtp() {
-//   return crypto.randomInt(100000, 1000000).toString();
-// }
-
-// console.log(generateSixDigitOtp());
-
-
-
 
 
