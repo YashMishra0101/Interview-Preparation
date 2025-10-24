a6e4956d2fbeb934a79d30111e5885e657d30219
YashMishra0101 <yashrkm0011@gmail.com>
2025-10-25 04:00:06 +0530

    Removed React Revision and Backend Learning from this repo, created separate repositories for each

--- react-section/react-revision/tanstack-query/src/components/index.js
+++
@@ -1,4 +0,0 @@
-export {default as TanStackDataFetch} from "../pages/TanStackDataFetch"
-export {default as AddInfo} from "../pages/AddInfo"
-export {default as NavBar} from "./NavBar" 
-export {default as EditInfo} from "../pages/EditInfo" 
