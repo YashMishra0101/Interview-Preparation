b4b33e1eb2e173f407ef63b9c8602f76a60128b1
YashMishra0101 <yashrkm0011@gmail.com>
2025-10-25 01:45:07 +0530

    Data modelling Learning Completed

--- Backend/Backend-Three/server.js
+++ Backend/Backend-Three/server.js
@@ -1,13 +1,13 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send('Server is running!');
+app.get("/", (req, res) => {
+  res.send("Server is running!");
 });
 
 app.listen(PORT, () => {
